59cd11308c314409ffb4f283b6bf44c7105649d9
Nastassja Swierczynski <nastassja.swierczynski@gmail.com>
2019-05-21 19:23:35 +0200

    added tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,76 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# puts 'Cleaning database...'
-# Restaurant.destroy_all
+puts 'Destory all doses'
 
-# puts 'Creating restaurants...'
-# cocktails_attributes = [
-#   {
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
+Dose.destroy_all
+
+puts 'Destory all ingredients'
+
+Ingredient.destroy_all
+
+puts 'Destory all cocktails'
+
+Cocktail.destroy_all
+
+puts 'Creating Ingredients'
+lemon = Ingredient.create!(name: 'lemon')
+ice = Ingredient.create!(name: 'ice')
+rhum = Ingredient.create!(name: 'rhum')
+mint_leaves = Ingredient.create!(name: 'mint leaves')
+strawberry = Ingredient.create!(name: 'strawberry')
+
+puts 'Creating Cocktails'
+strawberry_mojito = Cocktail.create!(name: 'Strawberry mojito')
+classic_mojito = Cocktail.create!(name: 'Classic mojito')
+
+puts "created #{Cocktail.count} cocktails"
+
+puts 'Creating Doses'
+
+#Strawberry Mojito
+Dose.create!({
+  description: "Add 10 cubes",
+  ingredient: ice,
+  cocktail: strawberry_mojito
+})
+Dose.create!({
+  description: "Add 2 slices",
+  ingredient: lemon,
+  cocktail: strawberry_mojito
+})
+
+Dose.create!({
+  description: "Mix frozen berries",
+  ingredient: strawberry,
+  cocktail: strawberry_mojito
+})
+
+Dose.create!({
+  description: "Pour half a glass",
+  ingredient: rhum,
+  cocktail: strawberry_mojito
+  })
+
+#Classic Mojito
+Dose.create!({
+  description: "Add 10 cubes",
+  ingredient: ice,
+  cocktail: classic_mojito
+})
+Dose.create!({
+  description: "Add 2 slices",
+  ingredient: lemon,
+  cocktail: classic_mojito
+})
+
+Dose.create!({
+  description: "Mix a handful of leaves",
+  ingredient: mint_leaves,
+  cocktail: classic_mojito
+})
+
+Dose.create!({
+  description: "Pour half a glass",
+  ingredient: rhum,
+  cocktail: classic_mojito
+})
+
+puts 'Finish'
