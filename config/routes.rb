4c2ba43e48fbf78e9f3d4de412ac408e78e0102e
Nastassja Swierczynski <nastassja.swierczynski@gmail.com>
2019-05-22 16:34:59 +0200

    update root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cocktail#index'
   resources :cocktails, only: [:index, :show, :create, :new] do
       resources :doses, only: [:new, :create]
     end
